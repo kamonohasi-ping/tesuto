428a45c92e1f8f9e1d6a8ce553e0283b70fea7bf
kamonohasi-ping <hawking0108@icloud.com>
2025-05-22 08:15:54 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 # Streamlitライブラリをインポート
-import streamlit as 
+import streamlit as st
 st.title('自己紹介')
 st.write('まずは名前を教えて')
 
 user_name = st.text_input
 
-st.header('あなたの名前は+str(user_name)+'です')
+st.header('あなたの名前は'+str(user_name)+'です')
 
-h=st.number_input('身長を入力してください　(m)',value=1.70)
-w=st.number_input('体重を入力してください　(kg)',value=70)
+h=st.number_input('身長を入力してください(m)',value=1.70)
+w=st.number_input('体重を入力してください(kg)',value=70)
 
 bmi=w/(h**2)
 
 dbmi=round(bmi,2)
 
-st.header('あなたのBMIは+str(dbmi)+'です')
+st.header('あなたのBMIは'+str(dbmi)+'です')
