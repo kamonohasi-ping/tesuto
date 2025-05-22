17d711a5daffdc747bb454ca1e0983021652399e
kamonohasi-ping <hawking0108@icloud.com>
2025-05-22 08:14:50 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 # Streamlitライブラリをインポート
-import streamlit as st
+import streamlit as 
+st.title('自己紹介')
+st.write('まずは名前を教えて')
 
+user_name = st.text_input
+
+st.header('あなたの名前は+str(user_name)+'です')
+
+h=st.number_input('身長を入力してください　(m)',value=1.70)
+w=st.number_input('体重を入力してください　(kg)',value=70)
+
+bmi=w/(h**2)
+
+dbmi=round(bmi,2)
+
+st.header('あなたのBMIは+str(dbmi)+'です')
